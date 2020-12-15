de3792205151a083d68bb0ebc6262666f6177c1d
lelderbe <lelderbe@student.21-school.ru>
2020-12-15 15:42:19 +0300

    bit operations test: bit1count

---
+++ operations/main.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lelderbe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/15 15:02:54 by lelderbe          #+#    #+#             */
+/*   Updated: 2020/12/15 15:18:43 by lelderbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		bit1count(unsigned x)
+{
+	unsigned	count;
+
+	count = 0;
+	while (x)
+	{
+		count++;
+		x &= x - 1;
+	}
+	return (count);
+}
+
+int		main(void)
+{
+	/*
+	unsigned	x;
+
+	x = 7;
+	printf("%d\n", x);
+	printf("%d\n", x &= (x - 1));
+	*/
+
+	printf("%d\n", bit1count(3));
+	
+	return (0);
+}
