a4f368e0b3a34531be3034fa842841abbeac1630
lelderbe <lelderbe@student.21-school.ru>
2020-12-23 15:24:39 +0300

    bit fields by enum

---
+++ bits/bit_fields_by_enum/main.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lelderbe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 14:19:54 by lelderbe          #+#    #+#             */
+/*   Updated: 2020/12/23 15:23:50 by lelderbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+enum { KEYWORD = 01, EXTERNAL = 02, STATIC = 04 };
+
+void	print_bits(int n)
+{
+	char	buf[] = "00000000";
+	int		i;
+
+	i = 8;
+	while (n && --i)
+	{
+		if (n & 1)
+			buf[i] = '1';
+		n = n >> 1;
+	}
+	printf("%s\n", buf);
+}
+
+int		main(void)
+{
+	int	flags;
+
+	flags = 0;
+	flags = flags | EXTERNAL;
+	print_bits(flags);
+	flags |= flags | STATIC;
+	print_bits(flags);
+}
