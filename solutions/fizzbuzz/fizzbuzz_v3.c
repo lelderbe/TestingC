3066dd7373551b88e2d36ea02f2d667c7f169057
lelderbe <e51@ya.ru>
2021-02-01 22:16:08 +0300

    FizzBuzz v3

---
+++ solutions/fizzbuzz/fizzbuzz_v3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int		main(void)
+{
+	int		i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+		i++;
+	}
+	return (0);
+}
